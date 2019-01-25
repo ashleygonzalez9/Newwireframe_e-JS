fa111d061fde33b1e99dc956a5010aa49bf9c531
ashleygonzalez9 <37843561+ashleygonzalez9@users.noreply.github.com>
2019-01-25 15:33:40 -0800

    Add files via upload

---
+++ scriptnew.js
@@ -0,0 +1,29 @@
+// JavaScript is capitalized using "camel case": https://en.wikipedia.org/wiki/Camel_case
+// Check out the power of the classList property: https://www.w3schools.com/jsref/prop_element_classlist.asp
+
+// "el" stands for the "element" that was just clicked
+function addClass(el){
+	el.classList.add("green");
+
+}
+
+// "el" stands for the "element" that was just clicked
+function removeClass(el){
+	el.classList.remove("red");
+}
+
+function Sunset(el){
+document.getElementById("number1").src = ("image/sunset1.jpg");
+document.getElementById("number2").src = ("image/sunset2.jpg");
+document.getElementById("number3").src = ("image/sunset3.jpeg");
+} 
+function Grass(el){
+document.getElementById("number1").src = ("image/grass.jpg");
+document.getElementById("number2").src = ("image/sunset.jfif");
+document.getElementById("number3").src = ("image/flower.jpg");
+} 
+function night(el){
+document.getElementById("number1").src = ("image/leftpicure.PNG");
+document.getElementById("number2").src = ("image/middlepicture.png");
+document.getElementById("number3").src = ("image/right.png");
+} 
